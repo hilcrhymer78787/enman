7a197c917c796b917b047ae36c338c60ca969700
hilcrhymer78787 <hilcrhymer78787@gmail.com>
2021-08-17 13:19:35 +0900

    test

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="crud">
+        <ul>
+            <li v-for="(task,taskIndex) in tasks" :key="taskIndex">
+                <h2><input type="checkbox" v-model="task.emergency"> {{task.content}}</h2>
+                <h5>作成日：{{task.created_at}}</h5>
+                <h5>更新日：{{task.updated_at}}</h5>
+            </li>
+        </ul>
         <pre>{{$data}}</pre>
     </div>
 </template>
@@ -30,5 +37,10 @@ export default {
     button {
         margin: 0 20px 50px;
     }
+}
+li{
+    padding-bottom: 10px;
+    margin-bottom: 10px;
+    border-bottom: 1px solid black;
 }
 </style>
