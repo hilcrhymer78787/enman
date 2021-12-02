0dd3272acb2b753f46cfd2572fb710f3da37b235
今野龍之介 <hilcrhymer78787@gmail.com>
2021-12-02 17:54:57 +0900

    dist 変更

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -114,9 +114,9 @@ export default {
     build: {
     },
 
-    generate: {
-        dir: '../../../enman.magicgifted.com'
-    },
+    // generate: {
+    //     dir: '../../../enman.magicgifted.com'
+    // },
 
     env: envSet,
 }
