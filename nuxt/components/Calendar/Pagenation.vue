a21018b17083f813bbd03258f7b52be28cb46256
今野龍之介 <hilcrhymer78787@gmail.com>
2021-12-12 10:28:44 +0900

    ページャー 修正

--- nuxt/components/Calendar/Pagenation.vue
+++ nuxt/components/Calendar/Pagenation.vue
@@ -2,11 +2,11 @@
     <v-toolbar color="teal" dark style="box-shadow:none;">
         <v-spacer></v-spacer>
         <div class="d-flex">
-            <v-btn @click="onClickPrevMonth()" icon>
+            <v-btn :to="`/calendar?year=${this.beforeYear}&month=${this.beforeMonth}`" icon>
                 <v-icon style="font-size:30px;">mdi-chevron-left</v-icon>
             </v-btn>
-            <h1>{{ $route.query.year }}年 {{ $route.query.month }}月</h1>
-            <v-btn @click="onClickNextMonth()" icon>
+            <h1>{{year }}年 {{month }}月</h1>
+            <v-btn :to="`/calendar?year=${this.nextYear}&month=${this.nextMonth}`" icon>
                 <v-icon style="font-size:30px;">mdi-chevron-right</v-icon>
             </v-btn>
         </div>
@@ -15,33 +15,34 @@
 </template>
 
 <script>
+import moment from "moment";
 export default {
-    methods: {
-        onClickPrevMonth() {
-            if (this.$route.query.month == 1) {
-                this.$router.push(
-                    `/calendar?year=${Number(this.$route.query.year) - 1}&month=12`
-                );
-            } else {
-                this.$router.push(
-                    `/calendar?year=${this.$route.query.year}&month=${
-                        Number(this.$route.query.month) - 1
-                    }`
-                );
-            }
+    computed: {
+        year() {
+            return this.$route.query.year;
         },
-        onClickNextMonth() {
-            if (this.$route.query.month == 12) {
-                this.$router.push(
-                    `/calendar?year=${Number(this.$route.query.year) + 1}&month=1`
-                );
-            } else {
-                this.$router.push(
-                    `/calendar?year=${this.$route.query.year}&month=${
-                        Number(this.$route.query.month) + 1
-                    }`
-                );
-            }
+        month() {
+            return this.$route.query.month;
+        },
+        nextYear() {
+            return moment(this.year + "-" + this.month)
+                .add(1, "months")
+                .format("Y");
+        },
+        nextMonth() {
+            return moment(this.year + "-" + this.month)
+                .add(1, "months")
+                .format("M");
+        },
+        beforeYear() {
+            return moment(this.year + "-" + this.month)
+                .subtract(1, "months")
+                .format("Y");
+        },
+        beforeMonth() {
+            return moment(this.year + "-" + this.month)
+                .subtract(1, "months")
+                .format("M");
         },
     },
 };
