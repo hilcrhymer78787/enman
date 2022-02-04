ce35a0b7e4f7656bb97f103349089a0bcfd7626d
今野龍之介 <hilcrhymer78787@gmail.com>
2022-02-04 19:15:05 +0900

    test

--- laravel/database/seeders/worksTableSeeder.php
+++ laravel/database/seeders/worksTableSeeder.php
@@ -25,6 +25,24 @@ class worksTableSeeder extends Seeder
                     'work_minute' => 15,
                 ],
             ]);
+            DB::table('works')->insert([
+                [
+                    'work_room_id' => 1,
+                    'work_date' => date('Y-m-d', strtotime("-${day} day")),
+                    'work_task_id' => $i % 2 + 3,
+                    'work_user_id' => 3,
+                    'work_minute' => 15,
+                ],
+            ]);
+            DB::table('works')->insert([
+                [
+                    'work_room_id' => 1,
+                    'work_date' => date('Y-m-d', strtotime("-${day} day")),
+                    'work_task_id' => 5,
+                    'work_user_id' => $i % 3 + 1,
+                    'work_minute' => 10,
+                ],
+            ]);
         }
         for ($i = 1; $i  < 200; $i = $i + 2) {
             $day = $i - 100;
@@ -35,30 +53,6 @@ class worksTableSeeder extends Seeder
                     'work_task_id' => 2,
                     'work_user_id' => 2,
                     'work_minute' => 30,
-                ],
-            ]);
-        }
-        for ($i = 1; $i  < 200; $i++) {
-            $day = $i - 100;
-            DB::table('works')->insert([
-                [
-                    'work_room_id' => 1,
-                    'work_date' => date('Y-m-d', strtotime("-${day} day")),
-                    'work_task_id' => $i % 2 + 3,
-                    'work_user_id' => 3,
-                    'work_minute' => 15,
-                ],
-            ]);
-        }
-        for ($i = 1; $i  < 200; $i++) {
-            $day = $i - 100;
-            DB::table('works')->insert([
-                [
-                    'work_room_id' => 1,
-                    'work_date' => date('Y-m-d', strtotime("-${day} day")),
-                    'work_task_id' => 5,
-                    'work_user_id' => $i % 3 + 1,
-                    'work_minute' => 10,
                 ],
             ]);
         }
